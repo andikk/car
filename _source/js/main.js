02a751d64a07c92f0d4f36d06b1a13c0ee348721
unknown <andikk@mail.ru>
2019-07-30 23:28:23 +0300

    Правки перед формированием модального окна

--- _source/js/main.js
+++ _source/js/main.js
@@ -1,6 +1,17 @@
 $(document).ready(function() {
 
+  var navMain = document.querySelector('.js-menu');
+  var navToggle = document.querySelector('.js-menu__button');
 
+  navToggle.addEventListener('click', function() {
+    if (navMain.classList.contains('js-menu_closed')) {
+      navMain.classList.remove('js-menu_closed');
+      navMain.classList.add('js-menu_opened');
+    } else {
+      navMain.classList.add('js-menu_closed');
+      navMain.classList.remove('js-menu_opened');
+    }
+  });
 
 
 });
